f186728e538357c74eee616da9957f0e1f0942d7
Thabo <justicethabo21@gmail.com>
2024-04-01 09:23:16 +0200

    Added a simple controller

---
+++ src/main/java/za/ac/cput/controller/HelloController.java
@@ -0,0 +1,14 @@
+package za.ac.cput.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/Hello")
+    public String sayHi(){
+        return  "Hi";
+    }
+}
