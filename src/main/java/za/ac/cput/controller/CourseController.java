c60755b8d7dc25911f288a23b99a3126a5557c9a
Thabo <justicethabo21@gmail.com>
2024-04-01 20:44:10 +0200

    Search by ID added

--- src/main/java/za/ac/cput/controller/CourseController.java
+++ src/main/java/za/ac/cput/controller/CourseController.java
@@ -1,23 +1,29 @@
 package za.ac.cput.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import za.ac.cput.domain.Course;
+import za.ac.cput.service.CourseService;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
-public class Topic {
-@Autowired
-    private Topic topic;
-@RequestMapping("/topics")
-    public List<Course> getALlTopics(){
-        return Arrays.asList(
-                new Course("ADP3503","Application Development Practice 3","Java SpringBoot"),
-    new Course("ADF203","Application Development Fundamental 3","Data Structures"),
-    new Course("ADT001","Application Development  Theory 3","Software Engineering"));
+public class CourseController {
 
+@Autowired
+private CourseService courseService;
+    // It's a singleton,
+@RequestMapping("/courses")
+    public List<Course> getALlCourses(){
+        return courseService.getALlCourses();
+
+    }
+@RequestMapping("/courses/{id}")
+    public Course getCourse(@PathVariable String id){
+
+    return courseService.getCourse(id);
     }
 }
--- src/main/java/za/ac/cput/service/CourseService.java
+++ src/main/java/za/ac/cput/service/CourseService.java
@@ -1,2 +1,26 @@
-package za.ac.cput.service;public class CourseService {
+package za.ac.cput.service;
+
+import org.springframework.stereotype.Service;
+import za.ac.cput.domain.Course;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class CourseService {
+    //Creates instances of the class when the program starts
+
+    List<Course>  courses = Arrays.asList( new Course("ADP3","Application Development Practice 3","Java SpringBoot"),
+    new Course("ADF3","Application Development Fundamental 3","Data Structures"),
+    new Course("ADT3","Application Development  Theory 3","Software Engineering"));
+
+
+    public List<Course> getALlCourses(){
+        return  courses;
+    }
+
+    public Course getCourse(String id){
+
+        return courses.stream().filter( c ->c.getCourseId().equals(id)).findFirst().get();
+    }
 }
